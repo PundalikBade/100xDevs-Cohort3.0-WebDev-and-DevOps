51bc3e588ed93c38022f0bdbcbf8b42548d5adfc
Pundalik Bade <krishu2415@gmail.com>
2025-05-11 21:17:23 +0530

    Assignments

---
+++ Week 01 - Orientation,HTML,CSS,JS/Day 2 - Assignments/Assignment1.js
@@ -0,0 +1,9 @@
+// Create a variable for each of the following: your favorite color, your height in the centimeters,and whether you like pizza. Use appropriate variable declaretions(let,const,or var) Try logging it using console.log()
+
+const favColor = ["white","Navy Blue","Red"];
+const myHeight = "177.80 cm";
+const pizzaPerson = false;
+
+console.log(favColor);
+console.log(myHeight);
+console.log(pizzaPerson);
---
+++ Week 01 - Orientation,HTML,CSS,JS/Day 2 - Assignments/Assignment2.js
@@ -0,0 +1,10 @@
+// Write a function sum that finds the sum of two numbers. Side quest - Try passing in a instead of a number and see what happens?
+
+function addNumber(a,b)
+{
+	let sum  = a + b;
+	return sum;
+}
+
+let massage = addNumber("5",10);
+console.log(massage);
